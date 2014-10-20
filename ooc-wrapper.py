67b25cdd9d550271b9a50e25420488945d0b8a3e
lmc2179 <lmc2179@github.com>
2014-10-19 23:18:05 -0400

    first commit

---
+++ ooc-wrapper.py
@@ -0,0 +1,9 @@
+class OOCWrapper(object):
+    def __init__(self, model_type=None):
+        self.model_type = model_type
+
+    def fit(self, data_source, data_type):
+        pass
+
+    def predict(self, data_source, data_type):
+        pass
