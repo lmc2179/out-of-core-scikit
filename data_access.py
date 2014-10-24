145ddc557ad3a8337aa350aa72bc8906a49995d5
lmc2179 <lmc2179@github.com>
2014-10-23 23:54:30 -0400

    added option for multiple iterations during fitting

--- data_access.py
+++ data_access.py
@@ -9,14 +9,13 @@ class AbstractDataAccess(object):
 
 class TestDataAccess(object):
     def read(self, data_source, input_fields, output_field):
-        print 'Reading test data; data source ignored'
         test_data = {'A':[1,2,3,4], 'B':[0,0,0,0], 'C':[7,7,7,7], 'D':[1,2,3,4]}
         all_inputs = zip(*[test_data[f] for f in input_fields])
-        input_batches = all_inputs[:2], all_inputs[2:],all_inputs[:2], all_inputs[2:],all_inputs[:2], all_inputs[2:]
+        input_batches = all_inputs[:2], all_inputs[2:]
         if output_field is None:
             return input_batches
         all_outputs = [o for o in test_data[output_field]]
-        output_batches = all_outputs[:2], all_outputs[2:],all_outputs[:2], all_outputs[2:],all_outputs[:2], all_outputs[2:]
+        output_batches = all_outputs[:2], all_outputs[2:]
         return input_batches, output_batches
 
     def write(self, output_batches, target_file):
--- wrapper-test.py
+++ wrapper-test.py
@@ -6,5 +6,6 @@ from sklearn import linear_model
 class OOCWrapperTest(unittest.TestCase):
     def test_wrapper_end_to_end(self):
         wrap=ooc_wrapper.OOCWrapper(model_type=linear_model.SGDRegressor, input_fields=['A','B'], output_field='D')
-        wrap.fit(None, 'test')
+        wrap.fit(None, 'test', iterations=10000)
+        print 'Expecting [1, 2]; [3, 4]'
         wrap.predict(None, 'test', None, 'test')
