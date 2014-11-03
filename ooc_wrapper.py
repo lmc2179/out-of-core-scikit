8fdf1f4b6abd9b47fe760373ee63c24e3bbb8830
lmc2179 <louiscialdella@gmail.com>
2014-11-02 21:40:43 -0500

    refactored ooc wrapper to split read logic for training vs testing

--- ooc_wrapper.py
+++ ooc_wrapper.py
@@ -10,7 +10,7 @@ class OOCWrapper(object):
 
     def fit(self, data_type, data_source_dict, iterations=1):
         for i in range(iterations):
-            input_batches, output_batches = self._get_batches(data_source_dict, data_type, self.input_fields, self.output_field)
+            input_batches, output_batches = self._get_training_batches(data_source_dict, data_type, self.input_fields, self.output_field)
             [self.model.partial_fit(self._unpack_input_batch(i),self._unpack_output_batch(o)) for i,o in zip(input_batches, output_batches)]
 
     def _unpack_input_batch(self, batch):
@@ -21,18 +21,19 @@ class OOCWrapper(object):
         batch = [element for element in batch]
         return batch
 
-    def _get_batches(self, data_source, data_type, input_fields, output_field=None):
+    def _get_training_batches(self, data_source, data_type, input_fields, output_field):
         accessor = self.data_access_map[data_type]()
-        if output_field:
-            io_dict = accessor.read(data_source, inputs=input_fields, output=output_field)
-            return io_dict['inputs'], io_dict['output']
-        else:
-            return accessor.read(data_source, inputs=input_fields)['inputs']
+        io_dict = accessor.read(data_source, inputs=input_fields, output=output_field)
+        return io_dict['inputs'], io_dict['output']
 
     def predict(self, data_type, data_source_dict, target_type, target_data_dict):
-        input_batches = self._get_batches(data_source_dict, data_type, self.input_fields)
+        input_batches = self._get_test_batches(data_source_dict, data_type, self.input_fields)
         output_batches = self._predict_batches(input_batches)
         self._write_predictions(target_data_dict, target_type, output_batches)
+
+    def _get_test_batches(self, data_source, data_type, input_fields):
+        accessor = self.data_access_map[data_type]()
+        return accessor.read(data_source, inputs=input_fields)['inputs']
 
     def _predict_batches(self, input_batches):
         return (self.model.predict(list(i)) for i in input_batches)
